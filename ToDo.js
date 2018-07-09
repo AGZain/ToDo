e8388fc5a0244709f9793550529bc5c19cd64cff
AGZain <abdulzainulabedin@gmail.com>
2018-07-09 15:01:28 -0400

    updates

--- ToDo.js
+++ ToDo.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-// get our server running
 
-app.get('/',function(req,res) {
-  res.send('HELLO POOPALINGO');
-});
 
 
 app.listen(port, () => {
