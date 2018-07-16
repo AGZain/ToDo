de53a4d04749ab359d9ebc9c5db7a3d8fe218fbc
AGZain <abdulzainulabedin@gmail.com>
2018-07-16 15:55:18 -0400

    added some dummy data

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -1,7 +1,9 @@
+var test_data = [{item:'buy item'},{item:'do homework'},{item:'go to work'}];
+
 module.exports = function(app){
 
   app.get('/todo',function(req,resp){
-    resp.render('todo');
+    resp.render('todo',{todos: data});
 
   });
 
