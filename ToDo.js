17277475f9349146233f07df2ab48eac60db6548
AGZain <abdulzainulabedin@gmail.com>
2018-07-16 15:30:16 -0400

    worked on ejs.. view for to do app

--- ToDo.js
+++ ToDo.js
@@ -7,5 +7,7 @@ app.set('view engine', 'ejs'); //setting up template engine
 
 app.use(express.static('./public'));  //location for static files
 
+
+todoController(app);
 app.listen(3000);
 console.log('This application is running on port 3000');
