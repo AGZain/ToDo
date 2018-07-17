713ebfe26037466b119c0e7cc705321f5770604d
AGZain <abdulzainulabedin@gmail.com>
2018-07-17 16:14:31 -0400

    added ability to add new items to ToDo list

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -1,15 +1,16 @@
 var test_data = [{item:'buy item'},{item:'do homework'},{item:'go to work'}];
-
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 module.exports = function(app){
 
   app.get('/todo',function(req,resp){
-    resp.render('todo',{todos: data});
+    resp.render('todo',{todos: test_data});
 
   });
 
-  app.post('/todo',function(req,resp){
-
-
+  app.post('/todo',urlencodedParser,function(req,resp){
+      test_data.push(req.body);
+      resp.json(test_data);
   });
 
   app.delete('/todo',function(req,resp){
