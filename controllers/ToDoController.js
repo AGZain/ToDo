323555c892532dc1c0ce375a74e54e8f06992cbc
AGZain <abdulzainulabedin@gmail.com>
2018-07-20 16:06:55 -0400

    added functionality to store data in db

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose')
 
 //Connected to to db
 
-mongoose.connect('mongodb://test:testpassword1@ds243501.mlab.com:43501/todo');
+mongoose.connect('mongodb://test:testpassword1@ds243501.mlab.com:43501/todo');    //conecting to mlab
 
 //Creating schema
 
@@ -16,29 +16,47 @@ var todoSch  = new mongoose.Schema({
 //Creating model
 
 var Todo = mongoose.model('Todo',todoSch);
-var itemOne = Todo({item: 'buy car'}).save(function(err){
-  if (err) throw err;
-  console.log('item saved');
-});
+
+
+//test run of adding data to db
+// var itemOne = Todo({item: 'buy car'}).save(function(err){
+//   if (err) throw err;
+//   console.log('item saved');
+// });
 
 
 module.exports = function(app){
 
   app.get('/todo',function(req,resp){
-    resp.render('todo',{todos: test_data});
+    //resp.render('todo',{todos: test_data});
+
+    Todo.find({}, function(err,data){
+      if(err) throw err;
+      resp.render('todo',{todos: data});
+    });
 
   });
 
   app.post('/todo',urlencodedParser,function(req,resp){
-      test_data.push(req.body);
-      resp.json(test_data);
+      // test_data.push(req.body);
+      // resp.json(test_data);
+    var todo_new = Todo(req.body).save(function(err,data){
+      if (err) throw err;
+      resp.json(data);
+    });
   });
 
   app.delete('/todo/:item',function(req,resp){
-    test_data = test_data.filter(function(todo){
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+
+    Todo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+      if (err) throw err;
+      resp.json(data);
+
     });
-    resp.json(test_data);
+    // test_data = test_data.filter(function(todo){k
+    //   return todo.item.replace(/ /g, '-') !== req.params.item;
+    // });
+    // resp.json(test_data);
   });
 
 
