1e809a10b1734470b6e76d7ee1ee161087b26ac0
AGZain <abdulzainulabedin@gmail.com>
2018-07-23 16:49:43 -0400

    trying to add redirect functionality

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -1,29 +1,17 @@
-var test_data = [{item:'buy item'},{item:'do homework'},{item:'go to work'}];
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 var mongoose = require('mongoose')
 
 //Connected to to db
-
 mongoose.connect('mongodb://test:testpassword1@ds243501.mlab.com:43501/todo');    //conecting to mlab
 
 //Creating schema
-
 var todoSch  = new mongoose.Schema({
   item: String
 });
 
 //Creating model
-
 var Todo = mongoose.model('Todo',todoSch);
-
-
-//test run of adding data to db
-// var itemOne = Todo({item: 'buy car'}).save(function(err){
-//   if (err) throw err;
-//   console.log('item saved');
-// });
-
 
 module.exports = function(app){
 
@@ -32,7 +20,8 @@ module.exports = function(app){
 
     Todo.find({}, function(err,data){
       if(err) throw err;
-      resp.render('todo',{todos: data});
+      //resp.render('todo',{todos: data});
+      resp.redirect('/new');
     });
 
   });
@@ -42,8 +31,10 @@ module.exports = function(app){
       // resp.json(test_data);
     var todo_new = Todo(req.body).save(function(err,data){
       if (err) throw err;
-      resp.json(data);
+      //resp.json(data);
+      
     });
+    
   });
 
   app.delete('/todo/:item',function(req,resp){
@@ -53,11 +44,10 @@ module.exports = function(app){
       resp.json(data);
 
     });
-    // test_data = test_data.filter(function(todo){k
-    //   return todo.item.replace(/ /g, '-') !== req.params.item;
-    // });
-    // resp.json(test_data);
   });
 
+  app.get('/new',function(req,resp){
+    
 
+  });
 };
